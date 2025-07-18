5b55e71c14341845e629789aa1adaf91c360ec2c
MariniDwi <141748278+MariniDwi@users.noreply.github.com>
2025-07-18 21:33:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let controlIcon = document.getElementById("controlIcon");
+
+song.onloadedmetadata = function(){
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
+
+function playPause(){
+    if(controlIcon.classList.contains("fa-pause")){
+        song.pause();
+        controlIcon.classList.remove("fa-pause");
+        controlIcon.classList.add("fa-play");
+    }
+    else{
+        song.play();
+        controlIcon.classList.add("fa-pause");
+        controlIcon.classList.remove("fa-play");
+    }
+}
+
+song.addEventListener("timeupdate", () => {
+    progress.value = song.currentTime;
+});
+
+progress.oninput = function(){
+    song.currentTime = progress.value;
+}
+
+progress.onchange = function(){
+    song.play();
+    song.currentTime = progress.value;
+    controlIcon.classList.add("fa-pause");
+    controlIcon.classList.remove("fa-play");
+}
+
+
